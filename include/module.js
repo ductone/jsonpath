7451e3c345accfb8b285ef6a7aa6c3c479b083a6
alan-lee-12 <lee.alan.c12@gmail.com>
2023-09-19 21:44:18 +0000

    disable readFileSync of module.js in grammar.

--- include/module.js
+++ include/module.js
@@ -1,34 +1,36 @@
-var _ast = {
+const _ast = {
 
-  initialize: function() {
+  initialize: function () {
     this._nodes = [];
     this._node = {};
     this._stash = [];
   },
 
-  set: function(props) {
+  set: function (props) {
     for (var k in props) this._node[k] = props[k];
     return this._node;
   },
 
-  node: function(obj) {
+  node: function (obj) {
     if (arguments.length) this._node = obj;
     return this._node;
   },
 
-  push: function() {
+  push: function () {
     this._nodes.push(this._node);
     this._node = {};
   },
 
-  unshift: function() {
+  unshift: function () {
     this._nodes.unshift(this._node);
     this._node = {};
   },
 
-  yield: function() {
+  yield: function () {
     var _nodes = this._nodes;
     this.initialize();
     return _nodes;
   }
 };
+
+export default _ast
